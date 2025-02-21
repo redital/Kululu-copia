ad286dec1f5ccfedf46eb523171eabd19f5c8f41
Raffaello Ippolito <86683344+redital@users.noreply.github.com>
2025-02-21 19:26:50 +0100

    sort dates

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 flask_app_config = {
     "debug": os.environ.get("FLUSK_DEBUG_OPTION", True),
-    "host": os.environ.get("FLASK_HOST", "0.0.0.0"),
+    "host": os.environ.get("FLASK_HOST", "127.0.0.1"),
     "port": os.environ.get("FLASK_PORT", 5000),
     "ssl_context": os.environ.get("CONTEXT", "adhoc"),
 }
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 import base64
 from datetime import datetime
 from config import Config, flask_app_config
+from datetime import datetime
 
 app = Flask(__name__)
 UPLOAD_FOLDER = "static/uploads"
@@ -31,8 +32,24 @@ def upload():
 @app.route("/images")
 def get_images():
     """ Restituisce l'elenco delle immagini salvate """
-    images = [f for f in os.listdir(UPLOAD_FOLDER) if f.endswith(".png")][::-1]
+    images = [f for f in os.listdir(UPLOAD_FOLDER) if f.endswith(".png")]
+    images = ordina_foto(images)
     return jsonify(images)
+
+
+def date_parser(nome_foto):
+    stringa_data = nome_foto[6:-4]
+    data = datetime.strptime(stringa_data, '%Y%m%d_%H%M%S')
+    return data
+
+def ordina_foto(lista_foto):
+    dizionario_date = {i:date_parser(i) for i in lista_foto}
+
+    sort_dates = lambda x : dict(sorted(x.items(), key=lambda item: item[1]))
+
+    dizionario_date = sort_dates(dizionario_date)
+
+    return list(dizionario_date.keys())
 
 @app.route("/static/uploads/<filename>")
 def uploaded_file(filename):
@@ -40,3 +57,6 @@ def uploaded_file(filename):
 
 if __name__ == "__main__":
     app.run(**flask_app_config)
+
+
+
